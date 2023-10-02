0fa014176288e75b7dd7c41193c7638278eb1de9
Peter <pm@trayportgermany.com>
2023-10-02 21:57:09 +0100

    More token tests

--- tests/tokeniserTests.cpp
+++ tests/tokeniserTests.cpp
@@ -65,7 +65,7 @@ TEST(TokeniserTests, TestCommentOnLine)
     EXPECT_EQ(tokens[5].value(), "this is a comment");
 }
 
-TEST(TokeniserTests, TestTokenInfoIsCorrect)
+TEST(TokeniserTests, TestTokenInfoOnLetStatementIsCorrect)
 {
     std::string testFilename = "test.emd";
     ErrorHandler handler;
@@ -96,4 +96,107 @@ TEST(TokeniserTests, TestTokenInfoIsCorrect)
     EXPECT_EQ(tokens[4].info().value().File, testFilename);
     EXPECT_EQ(tokens[4].info().value().Line, 1);
     EXPECT_EQ(tokens[4].info().value().Pos, 11);
+}
+
+TEST(TokeniserTests, TestTokenInfoOnReturnStatementIsCorrect)
+{
+    std::string testFilename = "test.emd";
+    ErrorHandler handler;
+    const std::string source = "return 10;";
+    Tokeniser tokeniser(source, testFilename, handler);
+
+    const auto tokens = tokeniser.tokenise();
+    // Check number of tokens
+    EXPECT_EQ(tokens.size(), 3);
+
+    // Check token info
+    EXPECT_EQ(tokens[0].info().value().File, testFilename);
+    EXPECT_EQ(tokens[0].info().value().Line, 1);
+    EXPECT_EQ(tokens[0].info().value().Pos, 1);
+
+    EXPECT_EQ(tokens[1].info().value().File, testFilename);
+    EXPECT_EQ(tokens[1].info().value().Line, 1);
+    EXPECT_EQ(tokens[1].info().value().Pos, 8);
+
+    EXPECT_EQ(tokens[2].info().value().File, testFilename);
+    EXPECT_EQ(tokens[2].info().value().Line, 1);
+    EXPECT_EQ(tokens[2].info().value().Pos, 10);
+}
+
+TEST(TokeniserTests, TestTokenInfoOnReturnStatementWithCommentIsCorrect)
+{
+    std::string testFilename = "test.emd";
+    ErrorHandler handler;
+    const std::string source = "return 10; # A comment";
+    Tokeniser tokeniser(source, testFilename, handler);
+
+    const auto tokens = tokeniser.tokenise();
+    // Check number of tokens
+    EXPECT_EQ(tokens.size(), 4);
+
+    // Check token info
+    EXPECT_EQ(tokens[0].info().value().File, testFilename);
+    EXPECT_EQ(tokens[0].info().value().Line, 1);
+    EXPECT_EQ(tokens[0].info().value().Pos, 1);
+
+    EXPECT_EQ(tokens[1].info().value().File, testFilename);
+    EXPECT_EQ(tokens[1].info().value().Line, 1);
+    EXPECT_EQ(tokens[1].info().value().Pos, 8);
+
+    EXPECT_EQ(tokens[2].info().value().File, testFilename);
+    EXPECT_EQ(tokens[2].info().value().Line, 1);
+    EXPECT_EQ(tokens[2].info().value().Pos, 10);
+
+    EXPECT_EQ(tokens[3].info().value().File, testFilename);
+    EXPECT_EQ(tokens[3].info().value().Line, 1);
+    EXPECT_EQ(tokens[3].info().value().Pos, 12);
+}
+
+TEST(TokeniserTests, TestTokenInfoOnMultilineStatementWithCommentIsCorrect)
+{
+    std::string testFilename = "test.emd";
+    ErrorHandler handler;
+    const std::string source = "let x = 10;\nreturn 10; # A comment";
+    Tokeniser tokeniser(source, testFilename, handler);
+
+    const auto tokens = tokeniser.tokenise();
+    // Check number of tokens
+    EXPECT_EQ(tokens.size(), 9);
+
+    // Check token info
+    EXPECT_EQ(tokens[0].info().value().File, testFilename);
+    EXPECT_EQ(tokens[0].info().value().Line, 1);
+    EXPECT_EQ(tokens[0].info().value().Pos, 1);
+
+    EXPECT_EQ(tokens[1].info().value().File, testFilename);
+    EXPECT_EQ(tokens[1].info().value().Line, 1);
+    EXPECT_EQ(tokens[1].info().value().Pos, 5);
+
+    EXPECT_EQ(tokens[2].info().value().File, testFilename);
+    EXPECT_EQ(tokens[2].info().value().Line, 1);
+    EXPECT_EQ(tokens[2].info().value().Pos, 7);
+
+    EXPECT_EQ(tokens[3].info().value().File, testFilename);
+    EXPECT_EQ(tokens[3].info().value().Line, 1);
+    EXPECT_EQ(tokens[3].info().value().Pos, 9);
+
+    EXPECT_EQ(tokens[4].info().value().File, testFilename);
+    EXPECT_EQ(tokens[4].info().value().Line, 1);
+    EXPECT_EQ(tokens[4].info().value().Pos, 11);
+
+    EXPECT_EQ(tokens[5].info().value().File, testFilename);
+    EXPECT_EQ(tokens[5].info().value().Line, 2);
+    EXPECT_EQ(tokens[5].info().value().Pos, 1);
+
+    EXPECT_EQ(tokens[6].info().value().File, testFilename);
+    EXPECT_EQ(tokens[6].info().value().Line, 2);
+    EXPECT_EQ(tokens[6].info().value().Pos, 8);
+
+    EXPECT_EQ(tokens[7].info().value().File, testFilename);
+    EXPECT_EQ(tokens[7].info().value().Line, 2);
+    EXPECT_EQ(tokens[7].info().value().Pos, 10);
+
+    EXPECT_EQ(tokens[8].info().value().File, testFilename);
+    EXPECT_EQ(tokens[8].info().value().Line, 2);
+    EXPECT_EQ(tokens[8].info().value().Pos, 12);
 }
